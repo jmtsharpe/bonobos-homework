854ce21f62963433c6252c34c2913e2723c5c0a0
James <Jmtsharpe@gmail.com>
2017-04-14 17:27:55 -0400

    create models and migrations for inventory and products. load seed data from csv files

--- db/migrate/20170414211906_create_inventory_items.rb
+++ db/migrate/20170414211906_create_inventory_items.rb
@@ -1,7 +1,7 @@
-class Inventory < ActiveRecord::Migration
+class CreateInventoryItems < ActiveRecord::Migration
 	def change
-		create_table :inventory do |t|
-			t.string :product_id, null: false
+		create_table :inventory_items do |t|
+			t.integer :product_id, null: false
 			t.integer :waist, null: false
 			t.integer :length, null: false
 			t.string :style, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'products.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+	t = Product.new
+	t.product_name = row['product_name']
+	t.product_image = row['product_image']
+	t.product_description = row['product_description']
+
+	t.save
+	puts "#{t.product_name} saved"
+end
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'inventory.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+	t = InventoryItem.new
+	t.product_id = row['product_id']
+	t.waist = row['waist']
+	t.length = row['length']
+	t.style = row['style']
+	t.count = row['count']
+
+	t.save
+	puts "#{t.style} saved"
+end
