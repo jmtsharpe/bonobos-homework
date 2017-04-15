612af4f395b42393fdb95ddaba77b610cdfc18e4
James <Jmtsharpe@gmail.com>
2017-04-15 14:33:19 -0400

    add 'loading...' to root. add more styling. Add sorting by style => waist => length for inventory

--- app/assets/javascripts/homeLoader.js
+++ app/assets/javascripts/homeLoader.js
@@ -1,15 +1,18 @@
 function loadProducts(){
 
 	function createProductList(data){
+		var root = $("#root");
 		var productList = $("<ul>");
 		var products = "";
 
 		data.map(function(product){
 			var prodDisplay = $("<li class='product-section'>");
+
 			var prodShowOff = $("<div class='showoff group'>");
 			prodShowOff.append("<h1 class='product-title'>"  + product.product_name + "</h1>");
 			prodShowOff.append("<img src='" + product.product_image + "' class='display-img'>");
 			prodShowOff.append("<article class='prod-descript'>" + product.product_description + "</article>");
+			
 			prodDisplay.append(prodShowOff);
 
 			var inventory = createInventoryList(product);
@@ -21,12 +24,15 @@ function loadProducts(){
 
 		productList.append(products);
 
-		$("#root").append(productList);
+		$("#root").html(productList);
 	}
 
 	function createInventoryList(product){
 		var itemList = $("<ul class='inventory group'>");
 		var items = "";
+		product.inventory_items.sort(function(a, b){
+			return sortByStyle(a, b);
+		});
 		items += "<li class='inventory-headers'><div class=' collumn col1'>Style</div><div class='collumn col2'>Waist</div><div class='collumn col3'>Length</div><div class='collumn col4'>Count</div></li>";
 		product.inventory_items.map(function(item){
 			items += "<li class='inventory-item'><div class=' collumn col1'>" + item.style + "</div><div class='collumn col2'>" + item.waist + "</div><div class='collumn col3'>" + item.length + "</div><div class=' collumn col4'>" + item.count + "</div></li>";
@@ -35,6 +41,40 @@ function loadProducts(){
 		itemList.append(items);
 
 		return itemList
+	}
+
+	function sortByStyle(a, b){
+		var nameA=a.style, nameB=b.style;
+		if (nameA === nameB){
+			return sortByWaist(a, b);
+		} else if (nameA > nameB){
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+	function sortByWaist(a, b){
+		var nameA=a.waist, nameB=b.waist;
+		if (nameA === nameB){
+			return sortByLength(a, b);
+		} else if (nameA > nameB){
+			return 1;
+		} else {
+			return -1;
+		}
+	}
+
+	function sortByLength(a, b){
+		var nameA=a.length, nameB=b.length;
+		if (nameA < nameB){
+			return -1;
+		}
+		if (nameA > nameB){
+			return 1;
+		}
+
+		return 0;
 	}
 
 	$.ajax({
