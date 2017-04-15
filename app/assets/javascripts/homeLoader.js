d9e07271b041a908115a0ddca36f78a2ace4d626
James <Jmtsharpe@gmail.com>
2017-04-14 23:34:51 -0400

    add classes and styles for inventory list. Add images for products

--- app/assets/javascripts/homeLoader.js
+++ app/assets/javascripts/homeLoader.js
@@ -5,12 +5,36 @@ function loadProducts(){
 		var products = "";
 
 		data.map(function(product){
-			products += "<li>" + product.product_name + "</li>"
+			var prodDisplay = $("<li class='product-section'>");
+			var prodShowOff = $("<div class='showoff group'>");
+			prodShowOff.append("<h1>"  + product.product_name + "</h1>");
+			prodShowOff.append("<img src='" + product.product_image + "' class='display-img'>");
+			prodShowOff.append("<p>" + product.product_description + "</p>");
+			prodDisplay.append(prodShowOff);
+
+			var inventory = createInventoryList(product);
+
+			prodDisplay.append(inventory);
+
+			productList.append(prodDisplay);
 		})
 
 		productList.append(products);
 
 		$("#root").append(productList);
+	}
+
+	function createInventoryList(product){
+		var itemList = $("<ul class='inventory group'>");
+		var items = "";
+		items += "<li class='inventory-headers'><div class=' collumn col1'>Style</div><div class='collumn col2'>Waist</div><div class='collumn col3'>Length</div><div class='collumn col4'>Count</div></li>";
+		product.inventory_items.map(function(item){
+			items += "<li><div class=' collumn col1'>" + item.style + "</div><div class='collumn col2'>" + item.waist + "</div><div class='collumn col3'>" + item.length + "</div><div class=' collumn col4'>" + item.count + "</div></li>";
+		})
+
+		itemList.append(items);
+
+		return itemList
 	}
 
 	$.ajax({
